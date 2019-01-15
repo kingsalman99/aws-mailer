0a47571f1a7860a97c05ffa7f9a617780af417f1
rain@dear.org <rain@dear.org>
2019-01-15 21:37:27 +0200

    tests

--- tests/tests_awsmailer.py
+++ tests/tests_awsmailer.py
@@ -1,8 +1,13 @@
 import unittest
+from email.mime.text import MIMEText
+import email.utils
+
 from awsmailer import is_valid_email
 from awsmailer import read_parse_msg
 from awsmailer import read_already_notified
 from awsmailer import read_recipients_lists
+from awsmailer import should_skip
+from awsmailer import create_smtp_msg
 
 class TestAWSMailer(unittest.TestCase):
 
@@ -29,3 +34,24 @@ class TestAWSMailer(unittest.TestCase):
         recs = read_recipients_lists('tests/resources/contacts')
         self.assertTrue('123@lol.com' in recs)
         self.assertTrue('abc@lol.com' in recs)
+
+    def test_should_skip(self):
+        notified = {'skipme@dot.com' : 1}
+        self.assertTrue(should_skip('skipme@dot.com', notified))
+        self.assertTrue(should_skip('bademaildot.com', notified))
+
+    def test_create_smtp_msg(self):
+        recipient_list = ['rec1', 'rec2']
+        parts = ['plain_text', 'html']
+        msg = create_smtp_msg('subject', 'sendername', 'sender',
+                              recipient_list, parts[0], parts[1])
+        self.assertEqual(msg['subject'], 'subject')
+        self.assertEqual(msg['From'],
+                         email.utils.formataddr(('sendername', 'sender')))
+        self.assertEqual(msg['Bcc'], ','.join(recipient_list))
+        counter = 0
+        for part in msg.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            self.assertEqual(part.get_payload(), parts[counter])
+            counter += 1
